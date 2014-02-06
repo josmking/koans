7c71f36d7311a705ee8fb542570bd938b8a29768
josmking <joe@househappy.org>
2014-02-06 14:08:43 -0800

    2_6_14_Commit

--- triangle.rb
+++ triangle.rb
@@ -13,9 +13,21 @@
 # and
 #   about_triangle_project_2.rb
 #
+
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if (a || b || c) <= 0
+  	raise TriangleError	
+  elsif (a + b) <= c || (a + c) <= b || (b + c) <= a 
+  	raise TriangleError
+  elsif ((a == b) && (a == c))
+    :equilateral
+  elsif ((a==b) || (a == c) || (b == c))
+  	:isosceles
+  else
+  	:scalene
+  end
 end
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
